e0fa2adacd8275d571637a1d739d5665f29f0d7f
aikaterhine <catarina.edp@gmail.com>
2020-04-29 13:09:11 -0300

    Implementa restricao de acesso a tarefa privada e edicao/remocao apenas de tarefas do usuario proprietario

--- advanced-todos/imports/ui/routes/Welcome.js
+++ advanced-todos/imports/ui/routes/Welcome.js
@@ -4,23 +4,23 @@ import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
 
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 export default class Welcome extends Component {
 
   render() {
     // Just render a placeholder container that will be filled in
     return (
-      <span ref="container">
-          <div className="Home">
-            <div className="lander">
-              <h1>Home page</h1>
-              <p>A simple app showing react button click navigation</p>
-              <form>
-                <Button variant="btn btn-success" onClick={() => history.push('/Products')}>Click button to view products</Button>
-              </form>
-            </div>
+      <div className="container">
+        <header>
+          <div>
+              <Typography variant="h3" gutterBottom>
+                Seja Bem-Vindo!
+              </Typography>
           </div>
-    </span>
+        </header>
+      </div>
+
   );
   }
 }
